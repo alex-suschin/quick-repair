cd3d8db040521c6ef1ae23410f4e24f8b114f63e
alex-suschin <alex_suschin@mail.ru>
2020-12-08 11:31:21 +0300

    desk-full

--- build/js/main.js
+++ build/js/main.js
@@ -4,7 +4,7 @@ Number.isNaN = Number.isNaN || function(value) {
 
 $(function() {
 
-    jQuery("a.anchor").click(function() {
+    jQuery("a.scroll").click(function() {
         elementClick = jQuery(this).attr("href")
         destination = jQuery(elementClick).offset().top;
         jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 700);
@@ -43,5 +43,24 @@ $(function() {
         $(this).addClass('active');
         $('.elems-bug').find('.elem-bug').hide();
         $('#' + $(this).data('switch')).show();
+    });
+
+    $('.services-item').hover(function() {
+        $('.services-icons').find('img').hide();
+        $('#' + $(this).data('bug')).show();
+    });
+    $('.services-item').mouseleave(function() {
+        $('.services-icons').find('img').hide();
+    });
+    $(window).scroll(function() {
+        if ($(window).width() > '991') {
+            if ($(this).scrollTop() > 115) {
+                $('.header').addClass('fixed');
+                $('body').css('padding-top', '90px');
+            } else if ($(this).scrollTop() < 115) {
+                $('.header').removeClass('fixed');
+                $('body').css('padding-top', '0');
+            }
+        }
     });
 })
